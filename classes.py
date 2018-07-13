3e680a582b4d7cdc5ed208d28d515f0a717dfecf
Nathan-Booth <boothnj@gmail.com>
2018-07-13 09:00:27 -0500

    Create class framework and game outline

---
+++ classes.py
@@ -0,0 +1,69 @@
+"""
+Game
+  init
+
+  welcome rules
+
+  pick rounds
+
+  run round
+
+  stats (score, player moves)
+
+  round score
+
+  final score
+
+"""
+# TODO: add game versions, like:
+# traditional 
+# rock paper scissors spock lizard
+# running computer tournament
+class Game():
+  def __init__():
+    pass
+  
+  def play_rnd():
+    pass
+
+  def play_match():
+    pass
+
+  def match_stats():
+    pass
+
+# --- Player Classes ---
+class Player():
+  def __init__():
+    pass
+
+  def move():
+    pass
+
+  def recall_rnd():
+    pass
+  
+class Human(Player):
+  def __init__():
+    pass
+
+  def move():
+    pass
+
+# may need to add intermediate Computer player class
+class Rocker(Player)
+  def __init__():
+    pass
+
+class Randomizer(Player):
+  def __init__():
+    pass
+
+class Copycat(Player):
+  def __init__():
+    pass
+
+class Cycler(Player):
+  def __init__():
+    pass
+
---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Welcome player
+Explain rules
+
+choose round number from odd num >= 3
+randomly pick computer player
+
+play game
+  human = input().lower() in ["rock", "paper", "scissors"]
+    if invalid, try again
+  computer move
+  round winner
+  display running score (for loop)
+    if last round, then final score
+    else running score
+"""
