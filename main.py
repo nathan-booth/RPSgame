582a3883b176d5feb895f352ecf319126f07fc9a
Nathan Booth <boothnj@gmail.com>
2018-07-16 12:29:06 -0500

    Add detail to game outline

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 Welcome player
 Explain rules
 
-choose round number from odd num >= 3
-randomly pick computer player
-
-play game
-  human = input().lower() in ["rock", "paper", "scissors"]
-    if invalid, try again
-  computer move
-  round winner
-  display running score (for loop)
-    if last round, then final score
-    else running score
+1. choose round number from odd num >= 3
+2. randomly pick computer player
+3. Initialize class game
+4. play rounds; for each round:
+    A. get player moves
+        I. human move should be validated on case and spelling
+        II. human should try again until they input valid move
+    B. compute score
+    C. display running score
+    D. display opponents' previous move
 """
 import classes
 
