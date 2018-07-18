cc7c09b16fcf74cf75a580950d864a689a2d161b
Nathan Booth <boothnj@gmail.com>
2018-07-18 11:07:06 -0500

    style: Remove notes in main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-"""
-Welcome player
-Explain rules
-
-1. choose round number from odd num >= 1
-2. randomly pick computer player
-3. Initialize class game
-4. play rounds; for each round:
-    A. get player moves
-        I. human move should be validated on case and spelling
-        II. human should try again until they input valid move
-    B. compute score
-    C. display running score
-    D. display opponents' previous move
-"""
 import classes
-from random import randint
 
 if __name__ == '__main__':
     player_pool = {'human': classes.Human(),
