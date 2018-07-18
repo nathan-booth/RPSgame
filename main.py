cd5089ee9abaaf61f24e050a533ee545c13d4a66
Nathan Booth <boothnj@gmail.com>
2018-07-18 17:12:21 -0500

    refactor: Modularize the text and shorten line lengths

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import classes
 
+player_pool_text = """You may choose from the following player pool:
+  Human: you control the moves
+  Rocker: computer that always chooses rock
+  Randomizer: computer that moves randomly
+  Copycat: computer that imitates the opponent's previous move
+  Cycler: computer that cycles through rock, paper, and scissors"""
 
 if __name__ == '__main__':
     player_pool = {'human': classes.Human(),
@@ -7,12 +13,12 @@ if __name__ == '__main__':
                    'randomizer': classes.Randomizer(),
                    'copycat': classes.Copycat(),
                    'cycler': classes.Cycler()}
-
-    player1 = input("""Who is Player 1? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
-    player2 = input("""Who is Player 2? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
+    print(player_pool_text)
+    player1 = input("Who is Player 1?").lower()
+    player2 = input("Who is Player 2?").lower()
     while player1 not in player_pool or player2 not in player_pool:
-        player1 = input("""Who is Player 1? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
-        player2 = input("""Who is Player 2? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
+        player1 = input("Who is Player 1?").lower()
+        player2 = input("Who is Player 2?").lower()
 
     game = classes.Game(player_pool[player1], player_pool[player2])
     game.play_match()
