b928ecab251ac5d5082b2f1e263f5b223386791c
Nathan Booth <boothnj@gmail.com>
2018-07-18 11:04:42 -0500

    feat: Add user choice of players at game start

--- main.py
+++ main.py
@@ -17,5 +17,13 @@ import classes
 from random import randint
 
 if __name__ == '__main__':
-    game = classes.Game(classes.Copycat(), classes.Randomizer())
+    player_pool = {'human': classes.Human(),
+                   'rocker': classes.Rocker(),
+                   'randomizer': classes.Randomizer(),
+                   'copycat': classes.Copycat(),
+                   'cycler': classes.Cycler()}
+    player1 = input("""Who is Player 1? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
+    player2 = input("""Who is Player 2? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
+
+    game = classes.Game(player_pool[player1], player_pool[player2])
     game.play_match()
