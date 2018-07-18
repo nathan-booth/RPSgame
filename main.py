f754760d8b9fd9bd37db4a00cefea799089f69ff
Nathan Booth <boothnj@gmail.com>
2018-07-18 11:39:00 -0500

    style: Block code in main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import classes
 
+
 if __name__ == '__main__':
     player_pool = {'human': classes.Human(),
                    'rocker': classes.Rocker(),
                    'randomizer': classes.Randomizer(),
                    'copycat': classes.Copycat(),
                    'cycler': classes.Cycler()}
+                   
     player1 = input("""Who is Player 1? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
     player2 = input("""Who is Player 2? The choices are Human, Rocker, Randomizer, Copycat, and Cycler.""").lower()
 
